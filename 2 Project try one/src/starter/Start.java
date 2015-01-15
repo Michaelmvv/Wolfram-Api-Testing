80605c2bb36252232744a70bef3dbe407dad0c01
Michaelmvv <bukkit.michaelmvv@gmail.com>
2015-01-14 17:52:42 -0800

    Small test

--- 2 Project try one/src/starter/Start.java
+++ 2 Project try one/src/starter/Start.java
@@ -3,7 +3,7 @@ package starter;
 import gui.MeGUI;
 
 public class Start {
-	public static boolean LogResult = false;
+	public static boolean LogResult = true;
 
 	public static void main(String[] args) {
 		new MeGUI().makeGui();

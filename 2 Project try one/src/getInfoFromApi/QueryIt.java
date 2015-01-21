08845aff2f55a1e24c714a742f749b40cc2c4ed3
michealmaddy <michealmaddy@10.0.1.104>
2015-01-20 17:59:37 -0800

    DO STUFFS

--- 2 Project try one/src/getInfoFromApi/QueryIt.java
+++ 2 Project try one/src/getInfoFromApi/QueryIt.java
@@ -1,6 +1,9 @@
 package getInfoFromApi;
 
+import com.wolfram.alpha.WAPlainText;
+import com.wolfram.alpha.WAPod;
 import com.wolfram.alpha.WAQueryResult;
+import com.wolfram.alpha.WASubpod;
 
 public class QueryIt {
 	public static WAQueryResult query(String quary, String type) {
@@ -9,4 +12,27 @@ public class QueryIt {
 
 	}
 
+	public static String qs(String query, String type) {
+		WolfApi api = new WolfApi(type);
+		StringBuilder builder = new StringBuilder();
+
+		WAQueryResult result = api.quary(query);
+		for (WAPod pod : result.getPods()) {
+			if (!pod.isError()) {
+				builder.append(pod.getTitle());
+				builder.append("------------");
+				for (WASubpod subpod : pod.getSubpods()) {
+					for (Object element : subpod.getContents()) {
+						if (element instanceof WAPlainText) {
+							builder.append(((WAPlainText) element).getText());
+						}
+					}
+				}
+				System.out.println("");
+			}
+		}
+		String s = builder.toString();
+		return s;
+
+	}
 }
--- 2 Project try one/src/gui/MeGUI.java
+++ 2 Project try one/src/gui/MeGUI.java
@@ -2,22 +2,37 @@ package gui;
 
 import getInfoFromApi.QueryIt;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 
 public class MeGUI implements ActionListener {
 	JFrame frame = new JFrame("Michaels Wolframalfa Project");
 	JPanel panel = new JPanel();
 	JTextField quaryField = new JTextField("Pi");
 	JButton SerchButton = new JButton("Go");
+	JTextPane text = new JTextPane();
+	JTabbedPane tabbedPane = new JTabbedPane();
 
 	public void makeGui() {
-		frame.add(panel);
+		quaryField.setPreferredSize(new Dimension(200, 40));
+		SerchButton.setPreferredSize(new Dimension(100, 40));
+		frame.setSize(800, 300);
+
+		frame.add(tabbedPane);
+
+		tabbedPane.add(panel, 0);
+		tabbedPane.add(text, 1);
+		tabbedPane.setTitleAt(0, "Input");
+		tabbedPane.setTitleAt(1, "Output");
+
 		panel.add(quaryField);
 		panel.add(SerchButton);
 		SerchButton.addActionListener(this);
@@ -30,8 +45,8 @@ public class MeGUI implements ActionListener {
 		JButton buttonPressed = (JButton) arg0.getSource();
 
 		if (buttonPressed == SerchButton) {
-			System.out
-					.println(QueryIt.query(quaryField.getText(), "plaintext"));
+			text.setText(QueryIt.qs(quaryField.getText(), "plaintext"));
+			frame.pack();
 		}
 
 	}
